312eda68f1a595af7e3c0fa33300faf159b75ed4
NatalyMora21 <tatamora21gmail.com>
2021-09-22 21:45:32 -0500

    Extraction method Performed to assign result according to score

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -3,8 +3,8 @@ public class TennisGame1 implements TennisGame {
     
     private int scorePlayer1 = 0;
     private int scorePlayer2 = 0;
-    private String player1Name;
-    private String player2Name;
+    final String player1Name;
+    final String player2Name;
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -12,72 +12,83 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-
-
         if (playerName == "player1")
             scorePlayer1 += 1;
-
         if (playerName == "player2")
             scorePlayer2 += 1;
     }
 
     public String getScore() {
-        //String score = "";
         StringBuilder score= new StringBuilder();
         int tempScore=0;
         if (scorePlayer1==scorePlayer2)
         {
-            switch (scorePlayer1)
-            {
-                case 0:
-                        score.append("Love-All");
-                    break;
-                case 1:
-                        score.append("Fifteen-All");
-                    break;
-                case 2:
-                        score.append("Thirty-All");
-                    break;
-                default:
-                        score.append("Deuce");
-                    break;
-
-            }
+            resultEqualScore(score);
         }
         else if (scorePlayer1>=4 || scorePlayer2>=4)
         {
-            int minusResult = scorePlayer1-scorePlayer2;
-            if (minusResult==1) score.append("Advantage player1");
-            else if (minusResult ==-1) score.append("Advantage player2");
-            else if (minusResult>=2) score.append("Win for player1");
-            else score.append("Win for player2");
+            resultGreaterEqualScore(score);
         }
         else
         {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = scorePlayer1;
-                else {
-                    score.append("-"); tempScore = scorePlayer2;
-                }
-
-                switch(tempScore)
-                {
-                    case 0:
-                        score.append("Love");
-                        break;
-                    case 1:
-                        score.append("Fifteen");
-                        break;
-                    case 2:
-                        score.append("Thirty");
-                        break;
-                    case 3:
-                        score.append("Forty");
-                        break;
-                }
-            }
+            resultDifferentScore(score);
         }
         return score.toString();
+    }
+
+    private void resultDifferentScore(StringBuilder score) {
+        int tempScore;
+        for (int i = 1; i<3; i++)
+        {
+            if (i==1) tempScore = scorePlayer1;
+            else {
+                score.append("-"); tempScore = scorePlayer2;
+            }
+
+            switch(tempScore)
+            {
+                case 0:
+                    score.append("Love");
+                    break;
+                case 1:
+                    score.append("Fifteen");
+                    break;
+                case 2:
+                    score.append("Thirty");
+                    break;
+                case 3:
+                    score.append("Forty");
+                    break;
+                default:
+                    score.append("");
+            }
+        }
+    }
+
+    private void resultGreaterEqualScore(StringBuilder score) {
+        int minusResult = scorePlayer1-scorePlayer2;
+        if (minusResult==1) score.append("Advantage player1");
+        else if (minusResult ==-1) score.append("Advantage player2");
+        else if (minusResult>=2) score.append("Win for player1");
+        else score.append("Win for player2");
+    }
+
+    private void resultEqualScore(StringBuilder score) {
+        switch (scorePlayer1)
+        {
+            case 0:
+                    score.append("Love-All");
+                break;
+            case 1:
+                    score.append("Fifteen-All");
+                break;
+            case 2:
+                    score.append("Thirty-All");
+                break;
+            default:
+                    score.append("Deuce");
+                break;
+
+        }
     }
 }
