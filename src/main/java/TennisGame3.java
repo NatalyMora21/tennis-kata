22b6939d9f4529543654c06d218b714be71af7ef
NatalyMora21 <tatamora21gmail.com>
2021-09-24 01:27:16 -0500

    Adjusted conditional

--- src/main/java/TennisGame3.java
+++ src/main/java/TennisGame3.java
@@ -13,20 +13,21 @@ public class TennisGame3 implements TennisGame {
 
     public String getScore() {
         String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
+        if (p1 < 4 && p2 < 4 && (p1 + p2 != 6)) {
+            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
             s = p[p1];
             return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
-        } else {
-            if (p1 == p2)
-                return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
         }
+
+        if (p1 == p2)
+            return "Deuce";
+        s = p1 > p2 ? p1N : p2N;
+        return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+
     }
     
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if (playerName.equals("player1"))
             this.p1 += 1;
         else
             this.p2 += 1;
